9f876a3e15ff9a841b78ba162d300608594f8861
Jake-Fischer <Taylormade123!>
2021-05-08 13:26:27 -0500

    Typo in index.js of models fixed

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
+//Require sequalize
 let { Sequelize, DataTypes } = require('sequelize')
 
-let env = process.env.NODE_ENB || 'development' 
 //Look for environment variables and set them as env if there are none, use the development env
 //If this is running locally, develpoment should be the env variable, if it's running on heroku there ill be a process
+let env = process.env.NODE_ENV || 'development'
+
 
 let config = require(__dirname + '/../config.json')[env]
 
